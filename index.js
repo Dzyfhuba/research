d9bac8ea86b82af81ac3c7efbf8fd94cfa6b5767
Dzyfhuba <hafidz21ub@gmail.com>
2021-12-22 21:43:14 +0700

    dataset is ready

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-filename = 'C104_100.json';
+filename = 'solomon/C109_100.json';
 $(document).ready(function () {
     $.getJSON(filename, function (data) {
         json_data = data['instance']['network']['nodes']['node'][0]['cx'];
         nodes = data['instance']['network']['nodes']['node'];
         fleet = data['instance']['fleet']['vehicle_profile'];
         requests = data['instance']['requests']['request'];
+        network = data['instance']['network'];
+        info = data['instance']['info'];
         $('#data').html(JSON.stringify(fleet));
 
         out_nodes = "";
         out_fleet = "";
         out_requests = "";
+        out_network = "";
+        out_info = "";
 
         nodes.forEach(node => {
             out_nodes += `<tr><td>${node['_id']}</td><td>${node['cx']}</td><td>${node['cy']}</td><td>${node['_type']}</td></tr>`;
@@ -36,9 +40,23 @@ $(document).ready(function () {
             </tr>`
         });
         
+        out_network = `
+        <tr>
+        <td>${network['euclidean']}</td>
+        <td>${network['decimals']}</td>
+        </tr>`;
+
+        out_info = `
+        <tr>
+        <td>${info['dataset']}</td>
+        <td>${info['name']}</td>
+        </tr>`;
+
         $('#nodes tbody').html(out_nodes);
         $('#fleet tbody').html(out_fleet);
         $('#requests tbody').html(out_requests);
+        $('#network tbody').html(out_network);
+        $('#info tbody').html(out_info);
     });
 });
 
@@ -53,6 +71,14 @@ $('#download').on('click', function(e){
     });
     $('#requests').tableExport({
         fileName: `${filename.split(".")[0]}-requests`,
+        type: 'csv',
+    });
+    $('#network').tableExport({
+        fileName: `${filename.split(".")[0]}-network`,
+        type: 'csv',
+    });
+    $('#info').tableExport({
+        fileName: `${filename.split(".")[0]}-info`,
         type: 'csv',
     });
 });
